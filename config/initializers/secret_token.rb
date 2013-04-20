78fa22d941e8f213c6497df8ad9e17fe9ff89b4f
Will Hamilton <whamilton42@hotmail.com>
2013-04-20 16:29:38 +0100

    New Rails project.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Flashy::Application.config.secret_token = 'c4749327b37e2b0c990ec159cf4154db1cd786f64c67067bdb66092625d1572da40bd2d1391201e29d626339dd5d1ae810b338ddbf176a118c7bd3e5c0eadbfd'
